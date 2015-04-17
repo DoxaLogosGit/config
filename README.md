2e2ba75dab98ebcbbc9395ddf574562b6afb44bd
Jay Atkinson <jgatkinsn@gmail.com>
2015-04-17 14:24:28 -0500

    initial setup of stuff, still need the rest of my vim gunk

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # config
-My configuration for vim editing
+My configuration for vim editing and keep consistent across machines
+
+To setup a new machine:
+
+ > git clone https://github.com/jgatkinsn/config.git
+ > cd config
+ > sh pull_submodules.sh
+ > sh setup_system.sh
+
+
+
---
+++ setup_system.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+ln -s $CWD/vimrc $HOME/.vimrc
+ln -s $CWD/vimfiles $HOME/.vim
+ln -s $CWD/gitconfig $HOME/.gitconfig
+#ln -s $CWD/pam_environment $HOME/.pam_environment
+#ln -s $CWD/screenrc $HOME/.screenrc
+
+#touch ~/.bashrc
+
+#if ! grep -Fq "$CWD/bashrc" ~/.bashrc
+#then
+#    echo "" >> ~/.bashrc
+#    echo "# Added by jhood's config setup" >> ~/.bashrc
+#    echo "source $CWD/bashrc" >> ~/.bashrc
+#fi
+
