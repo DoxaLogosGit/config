1f233e4355afb335d3461b2ee6d0ce6baa9aaef1
Jay Atkinson <jgatkinsn@gmail.com>
2015-09-15 11:29:38 -0500

    adding z shell config

--- setup_system.sh
+++ setup_system.sh
@@ -6,6 +6,8 @@ ln -s $CWD/vimrc $HOME/.vimrc
 ln -s $CWD/vimfiles $HOME/.vim
 ln -s $CWD/gitconfig $HOME/.gitconfig
 ln -s $CWD/tmux.conf $HOME/.tmux.conf
+ln -s $CWD/zshrc $HOME/.zshrc
+
 pushd vimfiles/bundle/YouCompleteMe
 git submodule update --init  --recursive
 ./install.sh --clang-completer 
