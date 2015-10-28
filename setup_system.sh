7c0eaa32558e48aeea4d3961bc702984452da0e8
Jay Atkinson <jgatkinsn@gmail.com>
2015-10-28 17:33:47 -0500

    add more setup to script

--- setup_system.sh
+++ setup_system.sh
@@ -2,6 +2,17 @@
 
 CWD=$(pwd)
 
+#install prerequsites
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -yy python-dev
+sudo apt-get install -yy cmake
+sudo apt-get install -yy vim-gtk
+sudo apt-get install -yy git
+sudo apt-get install -yy zsh
+sudo apt-get install -yy wget
+sudo apt-get install -yy tmux=2.0-1~ppa1~t
+
 ln -s $CWD/vimrc $HOME/.vimrc
 ln -s $CWD/vimfiles $HOME/.vim
 ln -s $CWD/gitconfig $HOME/.gitconfig
@@ -10,6 +21,10 @@ ln -s $CWD/zshrc $HOME/.zshrc
 mkdir -p $HOME/.tmux/plugins
 ln -s $CWD/tpm $HOME/.tmux/plugins/tpm
 ln -s $CWD/tmux-sensible $HOME/.tmux/plugins/tmux-sensible
+
+pushd ../
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+popd
 
 pushd vimfiles/bundle/YouCompleteMe
 git submodule update --init  --recursive
