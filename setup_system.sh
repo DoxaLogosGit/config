63f557a6d14af7bac22252f9e016f2d68873be60
Jay Atkinson <jgatkinsn@gmail.com>
2015-10-28 12:09:38 -0500

    updating with tmux plugin manager

--- setup_system.sh
+++ setup_system.sh
@@ -7,20 +7,11 @@ ln -s $CWD/vimfiles $HOME/.vim
 ln -s $CWD/gitconfig $HOME/.gitconfig
 ln -s $CWD/tmux.conf $HOME/.tmux.conf
 ln -s $CWD/zshrc $HOME/.zshrc
+mkdir -p $HOME/.tmux/plugins
+ln -s $CWD/tpm $HOME/.tmux/plugins/tpm
+ln -s $CWD/tmux-sensible $HOME/.tmux/plugins/tmux-sensible
 
 pushd vimfiles/bundle/YouCompleteMe
 git submodule update --init  --recursive
 ./install.sh --clang-completer 
 popd
-#ln -s $CWD/pam_environment $HOME/.pam_environment
-#ln -s $CWD/screenrc $HOME/.screenrc
-
-#touch ~/.bashrc
-
-#if ! grep -Fq "$CWD/bashrc" ~/.bashrc
-#then
-#    echo "" >> ~/.bashrc
-#    echo "# Added by jhood's config setup" >> ~/.bashrc
-#    echo "source $CWD/bashrc" >> ~/.bashrc
-#fi
-
