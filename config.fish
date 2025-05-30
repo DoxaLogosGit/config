09961b61fb981b80835959ae2945ffa90d02cd95
Jay Atkinson <jgatkinsn@gmail.com>
2025-05-30 00:39:44 -0500

    add detection for wsl and set TERM accordingly

--- config.fish
+++ config.fish
@@ -4,12 +4,19 @@ end
 starship init fish | source
 set -gx PATH $PATH ~/projects/Odin ~/.local/bin ~/projects/ols 
 set -gx ODIN_ROOT  ~/projects/Odin
+set -gx EDTIOR nvim
 alias ll "eza --icons --color=always --color-scale=all -l --git"
+
+set name_list $(string split "-" $(uname -r))
+
+if string match -qi -- "WSL2" $name_list
+    set -gx TERM linux
+end
 
 function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+	if set cwd (command bat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
 		builtin cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
